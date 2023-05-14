99d29cc605c369b8d45b4f8af0357c0b72826191
Georgii.Aksenov <georgii.aksenov@ataccama.com>
2023-05-14 18:04:58 +0200

    update jdk

--- ts-news-service/src/main/main.go
+++ ts-news-service/src/main/main.go
@@ -1,39 +1,5 @@
 package main
 
-import (
-	"github.com/hoisie/web"
-	//"labix.org/v2/mgo"
-	//"labix.org/v2/mgo/bson"
-	//"fmt"
-)
-
-//const (
-//	MONGODB_URL = "ts-news-mongo:27017"
-//)
-
-//func getNews(val string) string {
-//
-
-//	session, err := mgo.Dial(MONGODB_URL)
-//	if err != nil {
-//		panic(err)
-//	}
-//	defer session.Close()
-//
-//	session.SetMode(mgo.Monotonic, true)
-//	// db := session.DB("xtest")
-//	// collection := db.C("xtest")
-//	c := session.DB("xtest").C("xtest")
-//
-
-//	 var personAll []News
-//	 err = c.Find(nil).All(&personAll)
-//	 for i := 0; i < len(personAll); i++ {
-//	 	fmt.Println("Person ", personAll[i].Name, personAll[i].Phone)
-//	 }
-//
-//}
-
 type News struct {
 	Title   string `bson:"Title"`
 	Content string `bson:"Content"`
