4dce8029e24e9c41487570b9477885876cfe78ae
Georgii.Aksenov <georgii.aksenov@ataccama.com>
2023-05-14 18:28:04 +0200

    Fix ts-news-service

---
+++ ts-news-service/go.mod
@@ -0,0 +1,3 @@
+module ts-news-service/app
+
+go 1.19
--- ts-news-service/main.go
+++ ts-news-service/main.go
@@ -1,19 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 type News struct {
 	Title   string `bson:"Title"`
 	Content string `bson:"Content"`
 }
 
-func hello(val string) string {
+func hello(w http.ResponseWriter, r *http.Request) {
 	var str = []byte(`[
                        {"Title": "News Service Complete", "Content": "Congratulations:Your News Service Complete"},
                        {"Title": "Total Ticket System Complete", "Content": "Just a total test"}
                     ]`)
-	return string(str)
+
+	//fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+
+	fmt.Fprintf(w, string(str))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-	web.Get("/(.*)", hello)
-	web.Run("0.0.0.0:12862")
+	http.HandleFunc("/", hello)
+	log.Fatal(http.ListenAndServe(":12862", nil))
 }
